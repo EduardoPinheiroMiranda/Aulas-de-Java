ef0fbdf19d04e7f32c31f5077dbfd9e683d2bc76
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-14 20:50:51 -0300

    Simulado_ex2

---
+++ Treino/simulado_ex2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class simulado_ex2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n,  soma = 0;
+        
+        System.out.println("Digite um numero inteiro positivo.");
+        n = input.nextInt();
+        input.close();
+
+        soma = (1/n);
+        for(int i=1;i<n;i++){
+            if(i%2!=0){
+                soma-= 1/(n-i);
+            }else{
+                soma+= 1/(n-i);
+            }
+            /*if(i==n){
+                if(i%2==0){
+                    soma += 1;
+                }else{ 
+                    soma -= 1;
+                }
+            }*/
+        }
+        System.out.println("\nO valor de soma é "+ (soma));
+    }
+    
+}
---
+++ Treino/simulado_ex3.java
@@ -0,0 +1,3 @@
+public class simulado_ex3 {
+    
+}
