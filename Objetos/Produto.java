e45b75f83abd8b2e37847673c0f72c6b90e6cb53
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2022-03-22 22:00:23 -0300

    Update Produto.java
    Finalizando o ex1 proposto em aula:
    referente a manipulação de objetos.

--- Objetos/Produto.java
+++ Objetos/Produto.java
@@ -1,24 +1,72 @@
+import java.util.Scanner;
 public class Produto{
     String nome;
-    double peso,preco;
+    double peso,preco,quantidade;
 
     void informacao(){
-        System.out.println("\nProduto:  "+nome);
-        if(peso>10){
-            System.out.println("Peso acima de 10kg, diminua a quantidade do produto");
+        if(peso*quantidade<=10){
+                System.out.println("*********************************************");
+                System.out.println("**** O Produto "+nome+" foi vendido.\n****");
+                System.out.println("**** Produto: "+nome);
+                    if(nome=="Picanha"){
+                        System.out.println("**** Peso: "+quantidade);
+                    }else{
+                        System.out.println("**** Peso: "+peso);
+                    }
+                System.out.print  ("**** Valor: "+preco*quantidade);
+                System.out.println("\n*********************************************");
         }else{
-            System.out.println("Peso:  "+peso+" kg");
-        }
-        System.out.println("Preço: "+ preco+" /kg");
-       
+            System.out.println("O produto exedeu os 10kg, tente novamente!!");
+        }  
     }
+    
+    static void totalCompra(double mat[][],double desc){
+        desc = mat[1][2]-(mat[1][2]*(desc/100));
+        if(mat[0][0]*mat[0][1]<=10 && mat[1][0]*mat[1][1]<=10){
+            System.out.println("****");
+            System.out.println("**** TOTAL ---->  "+(mat[0][2]+desc));
+            System.out.println("****");
+            System.out.println("*********************************************\n\n\n");
+        }
+    }
+
+
     public static void main(String[] args){
-        Produto p1;
+        double desc;
+        double compfinal[][];
+        compfinal = new double[2][3];
+        Scanner input = new Scanner(System.in);
+
+        Produto p1, p2;
         p1 = new Produto();
         p1.nome = "Arroz Prato Fino";
-        p1.peso = 10;
         p1.preco = 9.5;
+        System.out.println("Qual a quantidade de "+ p1.nome+" ?");
+        p1.quantidade = input.nextDouble();
+        p1.peso = 10;
+
+        p2 = new Produto();
+        p2.nome = "Picanha";
+        System.out.println("Qual o peso da Picanha ?");
+        p2.quantidade = input.nextDouble();
+        p2.peso = 1;
+        p2.preco = 23;
+        System.out.println("Qual o desconto da Picanha ?");
+        desc = input.nextDouble();
+        input.close();
+        
+
+        compfinal[0][0]=p1.peso;
+        compfinal[0][1]=p1.quantidade;
+        compfinal[0][2]=p1.preco * p1.quantidade;
+        compfinal[1][0]=p2.peso;
+        compfinal[1][1]=p2.quantidade;
+        compfinal[1][2]=p2.preco * p2.quantidade;
+
+        System.out.println("\n\n\n\n");
 
         p1.informacao();
+        p2.informacao();
+        totalCompra(compfinal,desc);
     }
 }
